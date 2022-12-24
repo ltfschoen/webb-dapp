6a0b00f9b16715c3e4237880346382e921d78b3f
Luke Schoen <ltfschoen@gmail.com>
2022-12-24 22:43:16 +0000

    wip

--- apps/stats-dapp/webpack.config.js
+++ apps/stats-dapp/webpack.config.js
@@ -338,7 +338,7 @@ function createWebpackBase(env, mode = 'production') {
 
     // https://webpack.js.org/configuration/dev-server/
     devServer: {
-      port: 3001,
+      port: process.env.PORT_STATS_DAPP,
       host: '0.0.0.0',
       compress: true,
       allowedHosts: 'all',
